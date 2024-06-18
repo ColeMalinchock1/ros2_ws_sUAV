1fa5859c4de0ba44291b91a2828d83ad43da7795
ColeMalinchock1 <malinchc@gmail.com>
2024-06-18 08:43:02 -0400

    Pushing newest version and output txt file

--- sUAV/lidar_node.py
+++ sUAV/lidar_node.py
@@ -1,31 +1,38 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, Image, LaserScan
 from cv_bridge import CvBridge
 import cv2 as cv
 import numpy as np
 import threading
+import csv
 
 frame = None
 br = CvBridge()
+obstacle_location = "None detected"
 
 def image_callback(msg):
-    print("Callback reached")
     global frame
-    frame = br.imgmsg_to_cv2(msg)
+    data = msg.data
+    width = msg.width
+    height = msg.height
+    steps = msg.point_step
+    frame = 1
+    print(data)
 
-def process_image(frame):
-    img = frame
+    
 
-    cv.imshow("Frame", img)
-    cv.waitKey(1)
+def process_frame(frame):
+    pass
+    #for data in len(frame.data):
+
 
 def main(args = None):
     global frame
 
     rclpy.init(args = args)
     node = Node("lidar_node")
-    img_subscription = node.create_subscription(PointCloud2, 'scan_3D', image_callback, 5)
+    img_subscription = node.create_subscription(PointCloud2, '/scan_3D', image_callback, 5)
 
     thread = threading.Thread(target=rclpy.spin, args=(node, ), daemon=True)
     thread.start()
@@ -37,8 +44,8 @@ def main(args = None):
 
 
         if frame is not None:
-            print("Frame received")
-            process_image(frame)
+            continue
+            #process_frame(frame)
         else:
             print("Frame is none")
         rate.sleep()
